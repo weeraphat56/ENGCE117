f1ea8555d39b02f29c1d4ce54a541998dce56a6c
weerapat boonma <weerapatboonma44@gmail.com>
2025-12-06 21:44:58 +0700

    Update code.c

--- ENGCE117/Lab1.4/code.c
+++ ENGCE117/Lab1.4/code.c
@@ -1,13 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h> 
+
+int GetSet( int ** ) ; 
 
 int main() {
-    char Name[50] ;
-    int  Age = 0 ;
-    printf( "Enter your name: " ) ;
-    scanf( "%s", Name );
-    printf( "Enter your age: " ) ;
-    scanf( "%d", &Age ) ;
-    printf( "- - - - - -\n" ) ;
-    printf( "Hello %s \n" , Name ) ;
-    printf( "Age = %d \n" , Age ) ;
-}//end main function
+    int *data = NULL; 
+    int num = 0;
+    int i = 0;
+
+    num = GetSet( &data );
+
+    if ( num > 0 && data != NULL ) {
+        // Print the array elements
+        for( i = 0 ; i < num ; i++ ) {
+            printf( "\n%d" , data[i] );
+        }
+
+        // Free the dynamically allocated memory
+        free(data);
+        data = NULL;
+    } 
+
+    return 0 ;
+} 
+
+int GetSet( int **data ) {
+    int num;
+    int i = 0;
+    
+    // Read the desired array size
+    if ( scanf( "%d" , &num ) != 1 ) {
+        // Error on size input format
+        return 0 ;
+    }
+
+    if ( num <= 0 ) {
+        // Error on size value
+        return 0 ;
+    }
+    
+    // Allocate memory using malloc based on the input size 'num'
+    *data = (int*) malloc( num * sizeof(int) ); 
+
+    if ( *data == NULL ) {
+        // Error on memory allocation
+        return 0;
+    }
+
+    // Read the array elements
+    for( i = 0 ; i < num ; i++ ) {
+        if ( scanf( "%d" , &(*data)[i] ) != 1 ) {
+             // Error on element input format
+             free(*data); 
+             *data = NULL;
+             return 0;
+        }
+    }
+
+    return num ;
+}
