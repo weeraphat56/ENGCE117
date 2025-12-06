cc97acf95d43d05130d366389f01d7c64d20b5e2
weerapat boonma <weerapatboonma44@gmail.com>
2025-12-06 21:08:52 +0700

    Refactor code to include dynamic array input

--- ENGCE117/Lab1.2/code.c
+++ ENGCE117/Lab1.2/code.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
+void GetSet( int ** , int * ) ;
+
 int main() {
-    char Name[50] ;
-    int  Age = 0 ;
-    printf( "Enter your name: " ) ;
-    scanf( "%s", Name );
-    printf( "Enter your age: " ) ;
-    scanf( "%d", &Age ) ;
-    printf( "- - - - - -\n" ) ;
-    printf( "Hello %s \n" , Name ) ;
-    printf( "Age = %d \n" , Age ) ;
-}//end main function
+    int *data, num;
+    GetSet( &data, &num ) ;
+    printf("%d", data[1] ) ;
+
+    for( int i = 0 ; i < num ; i++ ) {
+        printf("%d", data[ i ] ) ;
+    }
+
+    return 0 ;
+}
+
+void GetSet( int **data , int *num ) {
+    int i = 0 ;
+
+    if (scanf( "%d" , num ) != 1) {
+        return ;
+    }
+
+    for( i = 0 ; i < *num ; i++ ) {
+        scanf( "%d" , &data[ i ] ) ;
+    }
+}
