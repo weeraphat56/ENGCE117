13e4e7ded8455d6792939d106659bf37bf1bc896
weerapat boonma <weerapatboonma44@gmail.com>
2025-12-06 21:28:06 +0700

    Update code.c

--- ENGCE117/Lab1.3/code.c
+++ ENGCE117/Lab1.3/code.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h> 
+
+int *GetSet( int* ) ;
 
 int main() {
-    char Name[50] ;
-    int  Age = 0 ;
-    printf( "Enter your name: " ) ;
-    scanf( "%s", Name );
-    printf( "Enter your age: " ) ;
-    scanf( "%d", &Age ) ;
-    printf( "- - - - - -\n" ) ;
-    printf( "Hello %s \n" , Name ) ;
-    printf( "Age = %d \n" , Age ) ;
-}//end main function
+    int *data = NULL , num ; 
+
+    data = GetSet( &num ) ;
+
+    if ( data != NULL ) {
+        free(data) ; 
+    }
+
+    return 0 ;
+}
+
+int *GetSet( int *num ) {
+    if ( scanf( "%d" , num ) != 1 ) {
+        printf( "Array size input error\n" ) ;
+        return NULL ;
+    }
+
+    if ( *num <= 0 ) {
+        printf( "Array size must be positive number\n" ) ;
+        return NULL ;
+    }
+
+    int *arr = (int*) malloc( (*num) * sizeof(int) ) ; 
+    
+    if (arr == NULL) {
+        printf( "Memory allocation failed\n" ) ;
+        return NULL;
+    }
+    
+    int i = 0 ;
+
+    for( i = 0 ; i < *num ; i++ ) {
+        scanf( "%d" , &arr[ i ] ) ;
+    }
+
+    return arr ; 
+}
